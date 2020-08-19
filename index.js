68a5caddbe6f76a918cf5e938ac0b41e22bc7af7
Jaiprakash siyyadri <jaiprakash.siyyadri@gramener.org>
2020-08-19 16:52:42 +0530

    ENH: gitignore and minor change fixes

--- index.js
+++ index.js
@@ -4,28 +4,69 @@ const fs = require("fs");
 
 const server = http.createServer((req, res) => {
     console.log(req.url)
-    if(req.url == '/'){
-        fs.readFile(path.join(__dirname, "public", "index.html"), (err,content) => {
-            if(err) throw err;
-            res.writeHead(200, {"Content-Type": "text/html"})
-            res.end(content)
-        })
+    // if(req.url == '/'){
+    //     fs.readFile(path.join(__dirname, "public", "index.html"), (err,content) => {
+    //         if(err) throw err;
+    //         res.writeHead(200, {"Content-Type": "text/html"})
+    //         res.end(content)
+    //     })
+    // }
+    // if(req.url == '/about'){
+    //     fs.readFile(path.join(__dirname, "public", "about.html"), (err,content) => {
+    //         if(err) throw err;
+    //         res.writeHead(200, {"Content-Type": "text/html"})
+    //         res.end(content)
+    //     })
+    // }
+    // if(req.url == '/api/users'){
+    //     const users = {
+    //         "name": "bob",
+    //         "age": 30
+    //     }
+    //     res.writeHead(200, {"Content-Type": "application/json"})
+    //     res.end(JSON.stringify(users))
+    // }
+
+    //  build file paths
+    let filePath = path.join(__dirname, 'public', req.url === '/'? 'index.html': req.url)
+
+    // etensions of the file
+    let extname  =  path.extname(filePath)
+    let contentType = 'text/html';
+
+    // check the extension and set content type
+    switch (extname){
+        case '.html':
+            contentType = 'text/html'
+            break
+        case '.css':
+            contentType = 'css'
+            break
+        case '.png':
+            contentType = 'png'
+            break
     }
-    if(req.url == '/about'){
-        fs.readFile(path.join(__dirname, "public", "about.html"), (err,content) => {
-            if(err) throw err;
-            res.writeHead(200, {"Content-Type": "text/html"})
-            res.end(content)
-        })
-    }
-    if(req.url == '/api/users'){
-        const users = {
-            "name": "bob",
-            "age": 30
+
+    fs.readFile(filePath, (err,content) => {
+        if(err){
+            if(err.code == "ENOENT"){
+                // file not found
+                fs.readFile(path.join(__dirname, "public", "404.html"), (err, content) => {
+                    res.writeHead(200, {'Content-Type': "text/html"})
+                    res.end(content, 'utf8')
+                })
+            } else{
+                // server error
+                res.writeHead(500)
+                res.end(`server ${err.code}`)
+            }
+        } else{
+            // Success
+            res.writeHead(200, {'Content-Type': contentType})
+            res.end(content, 'utf8')
         }
-        res.writeHead(200, {"Content-Type": "application/json"})
-        res.end(JSON.stringify(users))
-    }
+    })
+    
 });
 
 const PORT = process.env.PORT || 5000
